5d995be1dfe2758395322f1f14a9b44c665700a2
saurabh908 <usersaurabh92@gmail.com>
2020-07-13 22:51:40 +0530

    Reducers Implemented

--- component/App.jsx
+++ component/App.jsx
@@ -4,15 +4,19 @@ import {Container, Box} from '@material-ui/core';
 import * as utility from '../utility';
 
 import Inventory from './Inventory';
-import AddItem from './AddItem';
-import TeaShops from './TeaShops';
+import PostForm from './PostForm';
+import AllPost from './AllPost';
+
 
 const App = () => {
   return (
-    <div>
+    <div  className="App">
       <Container>
-        <AddItem />
-        <TeaShops />
+      <div className="navbar">
+       <h2 className="center ">Post It</h2>
+       </div>
+        <PostForm />
+    <AllPost />
         <Inventory />
       </Container>
     </div>
--- component/TeaShops.jsx
+++ component/TeaShops.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Post from './Post';
+
+import EditComponent from './EditComponent';
+
 class TeaShops extends Component {
   render() {
     return (
     <div>
       <h1>All Posts</h1>
-      {console.log(this.props.posts)}
+      {this.props.posts.map((post) => (
+                    <div key={post.id}>
+                        {post.editing ? <EditComponent post={post} key={post.id} /> :
+                            <Post key={post.id} post={post} />}
+                    </div>
+                ))}
     </div>
     );
    }
