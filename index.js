f503e4328bfa2edf1784185615ed265d005df443
Saurabh Kumar <34212933+saurabh908@users.noreply.github.com>
2020-07-13 22:15:15 +0530

    added reducers

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ import "./style.css";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./component/App";
+import postReducer from "./reducers/postReducer";
+
+const store = createStore(postReducer);
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </Provider>,
   document.querySelector("#root")
 );
