2fbfc007dd0a6c423d4891959b731e136710b7c1
saurabh908 <usersaurabh92@gmail.com>
2020-07-13 22:28:13 +0530

    Added reducers and connected to store

--- component/TeaShops.jsx
+++ component/TeaShops.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class TeaShops extends Component {
   render() {
     return (
     <div>
       <h1>All Posts</h1>
+      {console.log(this.props.posts)}
     </div>
     );
    }
 }
-
-export default TeaShops;
+const mapStateToProps = (state) => {
+    return {
+        posts: state
+    }
+}
+export default connect(mapStateToProps)(TeaShops);; 
